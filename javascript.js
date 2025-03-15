df88b4e19d7dd3df9bdf260086daca89380d37db
Romain Gremaud <romaingremaud@mac.attlocal.net>
2025-03-15 09:35:42 -0400

    Add buildGrid function
    Buildgrid function added to etch-a-sketch.  Function properly builds boxes,
    but doesn't scale them correctly.  Need to apply flex formatting.

--- javascript.js
+++ javascript.js
@@ -4,7 +4,6 @@
 const container = document.querySelector("#container");
 
 function createBox(width,height,backgroundColor) {
-
     const box = document.createElement("box")
     box.classList.add("box");
     box.style.width = width + 'px';
@@ -13,4 +12,20 @@ function createBox(width,height,backgroundColor) {
     box.style.border = "1px solid black";
     container.appendChild(box);
     return box;
+}
+
+// create function to call a for loop to build grid
+// boxCount is numbers of rows/columns 
+// width and height of container is 500px
+// box dimensions to equal 500px/(boxCount+"px") in order to fill full grid
+
+function buildGrid(boxCount,targetWidth,targetHeight,color) {
+    let i = 0;
+    let width = targetWidth;
+    let height = targetHeight;
+    let backgroundColor = color;
+    for (i = 0; i <= boxCount ** 2; i++) {
+        createBox(width,height,backgroundColor);
+    }
+
 }
