29829a8e7d8178f45cf324013ee3914f740c35a8
Romain Gremaud <romaingremaud@mac.attlocal.net>
2025-03-15 22:26:48 -0400

    Add buttons and additional functionality
    Added three buttons to set grid size and color type.  Color type buttons are not currently functional.
    Grid button works, but buildGrid needs to be updated to clear existing grid when creating new grid.

--- javascript.js
+++ javascript.js
@@ -17,16 +17,20 @@ function createBox(width,height,backgroundColor) {
     return box;
 }
 
+
+
 // create function to call a for loop to build grid
 // boxCount is numbers of rows/columns 
 // width and height of container is 500px
 // box dimensions to equal 500px/(boxCount+"px") in order to fill full grid
+// Create a button for buildGrid
+// User will select the button which will then prompt for the boxCount and call the function
 
-function buildGrid(boxCount,color) {
+function buildGrid(boxCount) {
     let i = 0;
     let width = (500/boxCount)-2;
     let height = (500/boxCount)-2;
-    let backgroundColor = color;
+    let backgroundColor = "white";
     for (i = 0; i < boxCount ** 2; i++) {
         createBox(width,height,backgroundColor);
     }
@@ -41,4 +45,20 @@ function randomNumber(min, max) { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
   
- 
+// add event listener for button click on gridButton
+// on click open a prompt to enter a number between 16 and 100
+// prompt sets value of boxCount
+// runs buildGrid function to populate new grid
+
+
+// write a function to set color of all grids to white and reset to 16x16
+
+ buildGrid(16);
+
+ const button = document.querySelector('gridButton');
+
+ gridButton.addEventListener('click', function() {
+    let boxCount = prompt("Please enter an integer between 16 and 100.")
+    buildGrid(boxCount);
+ });
+  
